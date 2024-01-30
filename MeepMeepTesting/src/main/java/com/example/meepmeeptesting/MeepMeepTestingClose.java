053b6c57448df084ddc303679186581bc171f9c3
Ruth <bluenby5@gmail.com>
2024-01-30 15:15:09 -0500

    WEEEEE
    fixed position logiccccccccccccccc

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingClose.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingClose.java
@@ -9,25 +9,29 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTestingClose {
 
 
-    // Set true if robot starts on red side, false if robot starts on blue side
-    static boolean red = false;
 
-    static int s = red ? -1 : 1;
+    static int s = 1;
     static Pose2d startPose = new Pose2d(12.0, 60*s, Math.toRadians(-90.0 * s));
 
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(1000);
 
         double angle = 20.0;
+        int a = (angle < 0 ? -1 : 1);
         RoadRunnerBotEntity myBot;
 
         double tag = -7.5;
 
         if (!(angle >= -15.0 && angle <= 15.0)) {
-            double heading = startPose.getHeading() - Math.copySign(Math.toRadians(30.0), angle);
-
-            double x = startPose.getX() - Math.copySign(12.0, angle) + Math.copySign(9.0 * Math.cos(heading), angle);
-            double y = Math.copySign(24.0, startPose.getY()) + Math.copySign(9.0 * Math.sin(heading), startPose.getY());
+//            double heading = startPose.getHeading() - Math.toRadians(30.0) * a;
+//
+//            double x = startPose.getX() + 12.0 * a * s - 9.0 * Math.cos(heading);
+//            double y = 24.0 * s - 9.0 * Math.sin(heading);
+            double heading = startPose.getHeading() - Math.toRadians(30.0)*a;
+//              * s
+            double x = startPose.getX() - 12.0 * a * s - + 9.0 * Math.cos(heading);
+//             + s * a *
+            double y = 24.0 * s - 9.0 * Math.sin(heading);
 
 
             if (angle < -15.0) {
@@ -38,7 +42,7 @@ public class MeepMeepTestingClose {
 
             Pose2d spikePose = new Pose2d(x, y, heading);
 
-            Pose2d tagPose = new Pose2d(48.0, Math.copySign(36.0, startPose.getY()) + tag, 0.0);
+            Pose2d tagPose = new Pose2d(48.0, 36.0 * s + tag, 0.0);
 
 
             myBot = new DefaultBotBuilder(meepMeep)
@@ -52,7 +56,7 @@ public class MeepMeepTestingClose {
                                             .splineToSplineHeading(spikePose, spikePose.getHeading())
 //                                .forward(7)
 //                                .back(7)
-                                            .lineToSplineHeading(new Pose2d(startPose.getX(), Math.copySign(48.0, startPose.getY()), startPose.getHeading()))
+                                            .lineToSplineHeading(new Pose2d(startPose.getX(), 48.0 * s, startPose.getHeading()))
 //                                            .splineToSplineHeading(new Pose2d(startPose.getX()+5, Math.copySign(48.0, startPose.getY()), startPose.getHeading()), Math.toRadians(90.0))
                                             .splineToConstantHeading(new Vector2d(24, startPose.getY()), 0.0)
 //                                            .splineToSplineHeading(new Pose2d(24, startPose.getY(), startPose.getHeading()), 0.0)
@@ -64,11 +68,11 @@ public class MeepMeepTestingClose {
             double heading = startPose.getHeading();
 
             double x = startPose.getX();
-            double y = Math.copySign(29.0, startPose.getY());
+            double y = 29.0 * s;
 
             Pose2d spikePose = new Pose2d(x, y, heading);
 
-            Pose2d tagPose = new Pose2d(48.0, Math.copySign(36.0, startPose.getY()) + tag, 0.0);
+            Pose2d tagPose = new Pose2d(48.0, 36.0 * s + tag, 0.0);
             myBot = new DefaultBotBuilder(meepMeep)
                     // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                     .setConstraints(41, 41, Math.toRadians(180), Math.toRadians(180), 16)
@@ -80,11 +84,11 @@ public class MeepMeepTestingClose {
                                             .splineToSplineHeading(spikePose, spikePose.getHeading())
 //                                .forward(7)
 //                                .back(7)
-                                            .lineToSplineHeading(new Pose2d(startPose.getX(), Math.copySign(48.0, startPose.getY()), startPose.getHeading()))
+                                            .lineToSplineHeading(new Pose2d(startPose.getX(), 48.0 * s, startPose.getHeading()))
 //                                            .splineToSplineHeading(new Pose2d(startPose.getX()+5, Math.copySign(48.0, startPose.getY()), startPose.getHeading()), Math.toRadians(90.0))
                                             .splineToConstantHeading(new Vector2d(24, startPose.getY()), 0.0)
 //                                            .splineToSplineHeading(new Pose2d(24, startPose.getY(), startPose.getHeading()), 0.0)
-                                            .lineTo(new Vector2d(26.0, Math.copySign(60.0, startPose.getY())))
+                                            .lineTo(new Vector2d(24.0, startPose.getY()))
                                             .splineToSplineHeading(tagPose, Math.toRadians(0.0))
                                             .forward(2)
                                             .build()
--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingFar.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingFar.java
@@ -9,25 +9,24 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTestingFar {
 
 
-    // Set true if robot starts on red side, false if robot starts on blue side
-    static boolean red = true;
-
-    static int s = red ? -1 : 1;
-    static Pose2d startPose = new Pose2d(-36.0, 60*s, Math.toRadians(-90.0 * s));
+    static int s = 1;
+    static Pose2d startPose = new Pose2d(-35.0, 60*s, Math.toRadians(-90.0 * s));
 
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
-        double angle = -20.0;
+        double angle = 20.0;
+        int a = (angle < 0 ? -1 : 1);
         RoadRunnerBotEntity myBot;
 
         double tag = -7.5;
 
         if (!(angle >= -15.0 && angle <= 15.0)) {
-            double heading = startPose.getHeading() + Math.copySign(Math.toRadians(30.0), angle);
-
-            double x = startPose.getX() - Math.copySign(12.0, startPose.getY()) + Math.copySign(9.0 * Math.cos(heading), startPose.getY());
-            double y = Math.copySign(24.0, startPose.getY()) + Math.copySign(9.0 * Math.sin(heading), startPose.getY());
+            double heading = startPose.getHeading() - Math.toRadians(30.0)*a;
+//              * s
+            double x = startPose.getX() - 12.0 * a * s - + 9.0 * Math.cos(heading);
+//             + s * a *
+            double y = 24.0 * s - 9.0 * Math.sin(heading);
 
 
             if (angle < -15.0) {
@@ -38,7 +37,7 @@ public class MeepMeepTestingFar {
 
             Pose2d spikePose = new Pose2d(x, y, heading);
 
-            Pose2d tagPose = new Pose2d(48.0, Math.copySign(36.0, startPose.getY()) + tag, 0.0);
+            Pose2d tagPose = new Pose2d(48.0, 36.0 * s + tag, 0.0);
 
 
             myBot = new DefaultBotBuilder(meepMeep)
@@ -52,7 +51,7 @@ public class MeepMeepTestingFar {
                                             .splineToSplineHeading(spikePose, spikePose.getHeading())
 //                                .forward(7)
 //                                .back(7)
-                                            .lineToSplineHeading(new Pose2d(startPose.getX(), Math.copySign(48.0, startPose.getY()), startPose.getHeading()))
+                                            .lineToSplineHeading(new Pose2d(startPose.getX(), 48.0 * s, startPose.getHeading()))
 //                                            .splineToSplineHeading(new Pose2d(startPose.getX()+5, Math.copySign(48.0, startPose.getY()), startPose.getHeading()), Math.toRadians(90.0))
                                             .splineToConstantHeading(new Vector2d(-24, startPose.getY()), 0.0)
                                             .lineTo(new Vector2d(24.0, startPose.getY()))
@@ -65,11 +64,11 @@ public class MeepMeepTestingFar {
             double heading = startPose.getHeading();
 
             double x = startPose.getX();
-            double y = Math.copySign(29.0, startPose.getY());
+            double y = 29.0 * s;
 
             Pose2d spikePose = new Pose2d(x, y, heading);
 
-            Pose2d tagPose = new Pose2d(48.0, Math.copySign(36.0, startPose.getY()) + tag, 0.0);
+            Pose2d tagPose = new Pose2d(48.0, 36.0 * s + tag, 0.0);
             myBot = new DefaultBotBuilder(meepMeep)
                     // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                     .setConstraints(41, 41, Math.toRadians(180), Math.toRadians(180), 16)
@@ -81,7 +80,7 @@ public class MeepMeepTestingFar {
                                     .splineToSplineHeading(spikePose, spikePose.getHeading())
 //                                .forward(7)
 //                                .back(7)
-                                    .lineToSplineHeading(new Pose2d(startPose.getX(), Math.copySign(48.0, startPose.getY()), startPose.getHeading()))
+                                    .lineToSplineHeading(new Pose2d(startPose.getX(), 48.0 * s, startPose.getHeading()))
 //                                            .splineToSplineHeading(new Pose2d(startPose.getX()+5, Math.copySign(48.0, startPose.getY()), startPose.getHeading()), Math.toRadians(90.0))
                                     .splineToConstantHeading(new Vector2d(-24, startPose.getY()), 0.0)
                                     .lineTo(new Vector2d(24.0, startPose.getY()))
