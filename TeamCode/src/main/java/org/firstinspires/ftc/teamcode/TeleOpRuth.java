9dc9ec709d98375b7b2ae8da4e10fcbb39c9a839
Ruth <bluenby5@gmail.com>
2024-02-01 16:49:18 -0500

    awefoijef

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRuth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpRuth.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
 
 public class TeleOpRuth extends LinearOpMode {
     public DcMotorEx backleft;
@@ -30,6 +31,11 @@ public class TeleOpRuth extends LinearOpMode {
         hooklatch = hardwareMap.get(Servo.class, "hooklatch");
 
         return hook != null && hooklatch != null;
+    }
+
+    public boolean airplaineInit() {
+        airplane = hardwareMap.get(Servo.class, "airplane");
+        return airplane != null;
     }
 
     public boolean arminit() {
@@ -85,22 +91,68 @@ public class TeleOpRuth extends LinearOpMode {
     public void runOpMode() {
 
         boolean drive = driveInit();
-        boolean gate = gateinit();
-        boolean hook = hookInit();
+        boolean gateinit = gateinit();
+        boolean hookinit = hookInit();
         double latch = hooklatch.getPosition();
+        boolean apinit = airplaineInit();
+        double launch = airplane.getPosition();
 
         while (opModeInInit() && !isStopRequested()) {
             telemetry.addLine()
                     .addData("DRIVE", drive)
                     .addData("ARM", arminit())
-                    .addData("HOOK", hook)
-                    .addData("GATE", gate);
+                    .addData("HOOK", hookinit)
+                    .addData("AIRPLANE", apinit)
+                    .addData("GATE", gateinit);
             telemetry.update();
         }
 
         waitForStart();
 
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
 
+                double boost = Math.abs(3.0 - gamepad1.right_trigger * 2.0);
+
+                double y = -gamepad1.left_stick_y;
+                double x = gamepad1.left_stick_x;
+                double turn = gamepad1.right_stick_x;
+
+                double power = Math.hypot(x, y);
+                double theta = Math.atan2(y, x);
+
+                double sin = Math.sin(theta - Math.PI/4);
+                double cos = Math.cos(theta - Math.PI/4);
+                double max = Math.max(Math.abs(sin), Math.abs(cos));
+
+                double bl = (power * sin/max  + turn);// / boost;
+                double br = (power * cos/max  - turn);// / boost;
+                double fl = (power * cos/max  + turn);// / boost;
+                double fr = (power * sin/max  - turn);// / boost;
+
+                backleft.setPower(bl);
+                backright.setPower(fl);
+                frontleft.setPower(br);
+                frontright.setPower(fr);
+
+                if (gamepad1.a && airplane.getPosition() == launch) {
+                    airplane.setPosition(1 - airplane.getPosition());
+                }
+
+                if (gamepad1.b && hooklatch.getPosition() == latch) {
+                    hooklatch.setPosition(1 - hooklatch.getPosition());
+                }
+
+                double hookp = gamepad1.dpad_up ? 1 : (gamepad1.dpad_down ? -1 : 0);
+
+                hook.setPower(hookp);
+
+                double gatep = gamepad1.dpad_right ? 0.25 : (gamepad1.dpad_left ? -0.25 : 0);
+
+                gate.setPower(gatep);
+
+            }
+        }
 
     }
 
